51fa3f9da92a7a98fcd90eb37a11cd9f8ed7a338
lekwi6017-source <lekwi6017@gmail.com>
2025-08-22 14:01:56 +0800

    chore: add pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/NereoPhone/',   // 👈 把这里换成你 GitHub 仓库的名字，比如 '/little-phone/'
   plugins: [react()],
   resolve: {
     alias: {
